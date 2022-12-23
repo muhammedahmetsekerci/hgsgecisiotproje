9c0dcf5ce409310396b36aab22a1149405581ef6
muhammedahmetsekerci <96115180+muhammedahmetsekerci@users.noreply.github.com>
2022-12-23 06:00:31 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hgsgecisiotproje
+{
+    public partial class Form1 : Form
+    {
+        int bakiye;
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "hRhqH8Wl5bMv1unhUpXBWfnFuC9Thcu37zu4QId4",
+            BasePath = "https://iot2022test-f698d-default-rtdb.firebaseio.com/"
+        };
+
+        IFirebaseClient client;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string eventName = "iotproje1";
+            string secretKey = "ewnhNKulL_QLHdqooYhqKsY_Mr44msi-V7NxIc2Gr2X";
+
+            // Set up the HTTP client and request
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, $"https://maker.ifttt.com/trigger/{eventName}/with/key/{secretKey}?value1={100}");
+
+            // Send the request and handle the response
+            HttpResponseMessage response = client.SendAsync(request).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Success");
+                bakiye = bakiye - 100;
+            }
+            else
+            {
+                MessageBox.Show($"Error triggering event: {response.StatusCode}");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int tutar = Convert.ToInt32(textBox1.Text);
+            bakiye = client.Get("bakiye").ResultAs<int>();
+
+            bakiye = bakiye + tutar;
+            client.Set("bakiye", bakiye);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new FirebaseClient(config);
+
+        }
+    }
+}
